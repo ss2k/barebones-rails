b570f0f4929afe012c73c9f3f6956e5ac5a49da4
ss <git@push.com>
2014-03-23 14:49:30 -0400

    First version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Barebones::Application.config.secret_key_base = '165b7d18aaefc4d22d87df8842d9b85fbe3947b9a7c9e2ba15cfb00c53e7b5830f6c3f069c885fbfa3a51139acc4f93aca7cf1375f4479d33afb18e981db08a7'
